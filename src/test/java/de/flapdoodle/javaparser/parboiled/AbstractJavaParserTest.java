d59eeb27f572de01081965313214ed8392a5618a
Michael Mosmann <michael@mosmann.de>
2013-08-21 17:44:22 +0200

    minor refactoring

--- src/test/java/de/flapdoodle/javaparser/parboiled/AbstractJavaParserTest.java
+++ src/test/java/de/flapdoodle/javaparser/parboiled/AbstractJavaParserTest.java
@@ -21,6 +21,19 @@ package de.flapdoodle.javaparser.parboiled;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.CharBuffer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Assert;
+
+import com.google.common.base.Charsets;
 import com.google.common.base.Optional;
 
 import de.flapdoodle.javaparser.tree.Source;
@@ -28,6 +41,39 @@ import de.flapdoodle.javaparser.tree.Source;
 
 public abstract class AbstractJavaParserTest {
 	
+	protected static String classSourceFromMavenTestDirectory(Class<?> classWithSourceInClassPath) {
+		Path filePath = Paths.get("src", "test", "java").resolve(classAsFileName(classWithSourceInClassPath));
+		try {
+			return read(new FileInputStream(filePath.toFile()));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static String read(InputStream sourceAsStream) {
+		Assert.assertNotNull(sourceAsStream);
+	
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(sourceAsStream, Charsets.UTF_8))) {
+			StringBuilder sb = new StringBuilder();
+	
+			int read;
+			char[] cb = new char[512];
+			CharBuffer buf = CharBuffer.wrap(cb);
+	
+			while ((read = reader.read(cb)) > 0) {
+				sb.append(buf.subSequence(0, read));
+			}
+			return sb.toString();
+	
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static String classAsFileName(Class<?> classWithSourceInClassPath) {
+		return classWithSourceInClassPath.getName().replace('.', '/') + ".java";
+	}
+
 	protected Source parse(String sourceText) {
 		Optional<Source> result = JavaParser.asSource(sourceText);
 		assertTrue(result.isPresent());
